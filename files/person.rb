3f8698c494f17d9bb7ebb154a80f3f58f2976bd6
tiagorahal <rahal.aires@gmail.com>
2021-10-29 14:57:46 -0300

    Organize files

--- files/person.rb
+++ files/person.rb
@@ -3,7 +3,7 @@ require_relative 'rental'
 
 class Person
   attr_accessor :age, :name
-  attr_reader :id, rentals
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
