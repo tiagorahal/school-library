57f5ea53f4bb929fef20795fb3603ddd473ebb23
Erick Contreras <eri89--@hotmail.com>
2021-11-02 14:28:31 -0600

    Update main

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 require_relative 'modules/app'
-
+require 'json'
+require 'pry'
+​
 class InputProcess
   include AppFunctions
-
   def initialize(book_list, people_list, rentals_list)
     @book_list = book_list
     @people_list = people_list
@@ -12,11 +13,9 @@ class InputProcess
     @show.options
     start_app
   end
-
+​
   def start_app
     user_input = gets.chomp.to_i
-    feature / data
-
     case user_input
     when 1
       @show.books(@book_list)
@@ -31,15 +30,23 @@ class InputProcess
     when 6
       @show.rentals(@rentals_list)
     when 7
+      save_files
       return
     end
     main(@book_list, @people_list, @rentals_list)
   end
 end
-
+​
 def main(book_list_input = [], people_list_input = [], rentals_list_input = [])
-  book_list = book_list_input
-  people_list = people_list_input
-  rentals_list = rentals_list_input
-  InputProcess.new(book_list, people_list, rentals_list)
+  InputProcess.new(book_list_input, people_list_input, rentals_list_input)
 end
+​
+books = File.open('data/books.json', 'r+')
+people = File.open('data/people.json', 'r+')
+rentals = File.open('data/rentals.json', 'r+')
+​
+book_list = JSON[books.read]
+people_list = JSON[people.read]
+rentals_list = JSON[rentals.read]
+​
+main(book_list, people_list, rentals_list)
