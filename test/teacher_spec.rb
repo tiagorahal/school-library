4906de076065868e5d638852ee8e6aa6f613bebe
Erick Contreras <eri89--@hotmail.com>
2021-11-03 13:46:39 -0600

    Add teacher_spec.rb

---
+++ test/teacher_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../classes/teacher'
+​
+describe Teacher do
+  context 'Test Teacher class' do
+    specialization = 'Math'
+    age = 30
+    name = 'Trevor'
+    teacher = Teacher.new(specialization, age, name)
+​
+    it 'Should be an instance of Teacher' do
+      expect(teacher).to be_an_instance_of Teacher
+    end
+​
+    it 'Specialization should be equal to Math' do
+      expect(teacher.specialization).to eq 'Math'
+    end
+​
+    it 'Age should be equal to 30' do
+      expect(teacher.age).to eq 30
+    end
+​
+    it 'Name should be equal to Trevor' do
+      expect(teacher.name).to eq 'Trevor'
+    end
+​
+    it 'Teachers can always use services' do
+      expect(teacher.can_use_services?).to eq true
+    end
+  end
+end
